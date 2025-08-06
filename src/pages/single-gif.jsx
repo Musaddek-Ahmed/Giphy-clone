ad248f6086c9b8bc5111bd4f7ea1672fb6546e96
Musaddek-Ahmed <musaddek59@gmail.com>
2025-08-06 13:44:36 +0600

    latest

--- src/pages/single-gif.jsx
+++ src/pages/single-gif.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+const contentType = ["gifs", "stickers", "texts"]
 
 const SingleGif = () => {
+  const {type, slug} = useParams()
+  const [gif, setGif] = useState({})
+
+  useEffect(()=> {
+    if (!contentType.includes(type)) {
+      throw new Error("Invalid Content Type")
+    }
+  }, [])
+
   return (
     <div>SingleGif</div>
   )
